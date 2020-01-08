4b00fcb78ee9c79fd0e9fd9c7e3dd986e72bc210
Rosalind <rosalindjchang@gmail.com>
2020-01-07 17:48:43 -0800

    Initial commit

---
+++ js/transition.js
@@ -0,0 +1,21 @@
+/*eslint-env browser*/ 
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
+$(document).ready(function(){
+    
+	
+	$("#blob").click(function(){ 
+		window.location = "https://rosalindjchang.github.io/whattheblob/";
+     });
+	
+	$("#fine").click(function(){ 
+		window.location = "https://rosalindjchang.github.io/ImFine/index.html";
+     });
+	
+	$("#ok").click(function(){ 
+		window.location = "https://rosalindjchang.github.io/sadzineweb/";
+     });
+	
+
+});
